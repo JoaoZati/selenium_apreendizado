facd364a535de2c2fadd47bb8b7518e515b1591b
JoaoZati <joaozati@gmail.com>
2022-05-19 09:04:10 -0300

    Adicionando um login simples na pagina

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from selenium import webdriver
-# path = "/home/joao/chromedriver"
-path = "./chromedriver"
+from selenium.webdriver.common.keys import Keys
+from decouple import Config, RepositoryEnv
 
-driver = webdriver.Chrome(path)
+DOTENV_FILE = '.env'
+env_config = Config(RepositoryEnv(DOTENV_FILE))
 
-driver.get('https://techwithtim.net')
+PATH_CHROMEDRIVER = env_config('PATH_CHROMEDRIVER')
+PAGE_HTML = env_config('PAGE_HTML')
+EMAIL_LOGIN = env_config('EMAIL_LOGIN')
+PASSWORD_LOGIN = env_config('PASSWORD_LOGIN')
+
+driver = webdriver.Chrome(PATH_CHROMEDRIVER)
+
+driver.get(PAGE_HTML)
+
+print(driver.title)
+print(driver.page_source)
+
+element_email = driver.find_element_by_name("email")
+element_email.send_keys(EMAIL_LOGIN)
+
+element_senha = driver.find_element_by_name("password")
+element_senha.send_keys(PASSWORD_LOGIN)
+
+element_login = driver.find_element_by_id("submit")
+element_login.click()
+
+driver.close() # tab
+driver.quit()
--- readme.md
+++ readme.md
@@ -1,8 +1,18 @@
 ## Rodar Script
-- pip install pipenv # se nao tiver
-- pipenv install  # mesmo diretorio
-- pipenv shell
 
-## Caso arquivo nao seja executavel
+### Pipenv
+- pip install pipenv # se nao tiver
+- pipenv install  # mesmo diretorio (criar ambiente virtual)
+- pipenv shell # mesmo diretorio (rodar ambiente virtual)
+
+### Python Decouple
+- mude o nome do arquivo .env_sample para .env
+- insira o servidor que você quer testar (teste, local ou produção)
+- insira um email e password validos 
+
+### Caso arquivo nao seja executavel o chromedriver
 - sudo chomod +x ./chromedriver
-- instalar chrome
+- instalar chrome no seu pc
+
+### Rode o codigo
+- python main.py
